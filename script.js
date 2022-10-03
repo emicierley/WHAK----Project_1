d935ab55c510773191e45e0e3cf266d5b6c61499
emi <cierleefwork@gmail.com>
2022-10-03 11:58:41 -0700

    gameboard - added classes, id's, and visibility

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
+/////////////////////////////////
 //GLOBAL VARIABLES
-
+//let endGame= 0
+/////////////////////////////////
+//FUNCTION FOR GAME LOGIC
 // HOW TO DECLARE AN ARROW FUNCTION:
 // const functionName = (parameters) => {
-// code would go here
 //}
 
+//we are accessing the sq. & inside of the sq.
+const square = document.querySelectorAll('.square')
+const mole = document.querySelectorAll('m1')
+
+//cited source: Haritha Computers & Technology --https://youtu.be/HQNH2GBYL9k
+//this function makes the mole appear & disappear
+function outMole() {
+  document.getElementById('m1').style.visibility = 'visible'
+}
+function inMole() {
+  if (document.getElementById('m1').style.visibility === 'visible')
+    document.getElementById('m1').style.visibility = 'hidden'
+}
+setTimeout('outMole()', 500)
+setTimeout('inMole()', 800)
+
+////////////////////////////////
 //EVENT LISTENERS
 
-//code goes here
+//start/reset button
+// btn.addEventListener('click', resetBoard) //makes reset button work, ready for new game play
