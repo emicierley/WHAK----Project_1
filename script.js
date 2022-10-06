6e410cf23cc496842b97ceff204bded577a0934a
emi <cierleefwork@gmail.com>
2022-10-06 06:57:00 -0700

    added 2nd html and css page

--- script.js
+++ script.js
@@ -9,39 +9,41 @@ let gameOn = false
 
 //cited source: Haritha Computers & Technology --https://youtu.be/HQNH2GBYL9k
 // Moles are out of holes, set with a parameter to go into the hole
-function outMole(holes) {
-  document.getElementById(holes.id).style.visibility = 'visible'
+function outMole(hole) {
+  document.getElementById(hole.id).style.visibility = 'visible'
+  let randomNumber = Math.random() * (3000 - 1000) + 1000
+  //tells moles to pop IN hole every x milli-seconds
+  setTimeout(() => inMole(hole), randomNumber)
 }
 // Moles are clickable, when game is on they can get into the hole
-function inMole(holes) {
+function inMole(hole) {
   if (gameOn) {
-    if (document.getElementById(holes.id).style.visibility === 'visible') {
-      document.getElementById(holes.id).style.visibility = 'hidden'
-      scoreBoard++
-      console.log(scoreBoard)
-      //tells moles to pop out of holes every x milli-seconds
-      setTimeout(() => outMole(holes), 2000)
+    if (document.getElementById(hole.id).style.visibility === 'visible') {
+      document.getElementById(hole.id).style.visibility = 'hidden'
+      let randomNumber = Math.random() * (3000 - 1000) + 1000
+      //tells moles to pop OUT of hole every x milli-seconds
+      setTimeout(() => outMole(hole), randomNumber)
     }
   }
 }
-//start & set game play timer
+//When
 function gamePlay() {
   gameOn = true
   console.log('the game is starts')
   scoreBoard = 0
-  //TODO unfinished mole-- get moles to pop up
+  //hides moles and they each pop-up @ different times
   for (let i = 0; i < randomHoles.length; i++) {
     document.getElementById(randomHoles[i].id).style.visibility = 'hidden'
+    let randomNumber = Math.random() * (4000 - 1000) + 1000
     setTimeout(() => {
-      let randomMole = randomHoles[Math.floor(Math.random() * 9)]
-      outMole(randomMole)
-    }, 2000)
+      outMole(randomHoles[i])
+    }, randomNumber)
   }
   //tells moles game is over after x milli-seconds
   setTimeout(() => {
     gameOn = false
     console.log('the game is now over')
-  }, 15000)
+  }, 45000)
 }
 
 ////////////////////////////////
@@ -49,8 +51,21 @@ function gamePlay() {
 
 //click on the mole, it will go into hole
 // document.getElementById('m1').addEventListener('click', inMole)
-document.querySelectorAll('.hole').forEach((holes) => {
-  holes.addEventListener('click', () => inMole(holes))
+document.querySelectorAll('.hole').forEach((hole) => {
+  hole.addEventListener('click', () => {
+    inMole(hole)
+    if (gameOn) {
+      scoreBoard++
+      console.log(scoreBoard)
+    }
+  })
 })
 //start/reset button
 btn.addEventListener('click', gamePlay)
+
+const resetBoard = () => {
+  document.querySelectorAll(outMole).forEach((randomMole) => {
+    gameOver = false
+    console.log(resetBoard)
+  })
+}
